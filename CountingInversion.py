73debd7e05f07e12c023241be6c5aa84fd0b0f22
Douglas Castro <farias.douglascastro@gmail.com>
2023-11-20 23:29:04 -0300

    Update Counting Inversions

--- CountingInversion.py
+++ CountingInversion.py
@@ -1,3 +1,5 @@
+import random
+
 class CountingInversion:
     def __init__(self):
         self.total_inversions = 0
@@ -20,14 +22,12 @@ class CountingInversion:
         sorted_result += left[i:]
         sorted_result += right[j:]
 
-        print("Conquer")
-        print(sorted_result)
+        self.print_step("Conquer", sorted_result)
 
         return sorted_result
 
     def count_inversions(self, arr):
-        print("Divide")
-        print(arr)
+        self.print_step("Divide", arr)
         if len(arr) <= 1:
             return arr
 
@@ -39,19 +39,21 @@ class CountingInversion:
         combined_array = self.combine(left, right)
 
         return combined_array
+        
+    def print_step(self, message, arr):
+        print(message)
+        print(arr)
 
 def main():
     counting_inversions = CountingInversion()
+    with open('arrays.txt', 'r') as file:
+        arq = file.readlines()
 
-    n = int(input("Digite o tamanho da sequência: "))
-    seq = list(map(int, input("Digite a sequência separada por espaços: ").split()))
-
-    sorted_seq = counting_inversions.count_inversions(seq)
+    line = random.choice(arq)
+    arr = line.split()
+    sorted_seq = counting_inversions.count_inversions(arr)
     print("Total de inversões:", counting_inversions.total_inversions)
 
 if __name__ == "__main__":
     main()
 
-# Exemplo:    
-# 13
-# 2 3 5 7 10 11 1 4 6 8 9 12 13
